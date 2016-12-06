4a4c1dc73bb2c13c1e3c67e1c9b4fbcd82eea605
wicked <wicked@puppetlabs.vm>
2016-12-06 01:29:15 +0000

    Updated code to get it working

--- modules/review/manifests/init.pp
+++ modules/review/manifests/init.pp
@@ -3,22 +3,22 @@ class review (String $user='review')
     user { $user:
          name             => $user,
          ensure           => 'present',
-         shell            => '/bin/csh'
+         shell            => '/bin/csh',
      }
 
     file { '/etc/shells':
         owner  => 'root',
         group  => 'root',
         mode   => '0644',
-        source => 'puppet:///modules/review/shells'
+        source => 'puppet:///modules/review/shells',
     }
 
     file { '/etc/motd':
         ensure  => file,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0644',
-        content => epp('motd.epp')
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => epp('review/motd.epp'),
     }
 }
 
