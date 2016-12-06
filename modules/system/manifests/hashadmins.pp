f4061a72046a6c9d4d014c8bc53b13e6977ceebc
wicked <wicked@puppetlabs.vm>
2016-12-06 22:41:55 +0000

    Refactored to using hash, create_resources function and also Pupppet 4 loop

--- modules/system/manifests/hashadmins.pp
+++ modules/system/manifests/hashadmins.pp
@@ -1,22 +1,30 @@
 class system::hashadmins {
-  require mysql::server
+    require mysql::server
 
-  $mysql_defaults = {
-      'ensure' => 'present',
-      'max_queries_per_hour' => 600,
-  }
+    $mysql_defaults = {
+        'ensure'               => 'present',
+        'max_queries_per_hour' => 600,
+    }
 
-  $mysql_users = {
-    'monica@localhost' => {},
-    'brad@localhost'   => {},
-    'luke@localhost'   => {},
-    'ralph@localhost'  => {
-        'ensure' => 'absent',
-    },
-    'zack@localhost'   => {
-        'max_queries_per_hour' => 1200,
-    },
-  }
+    $mysql_users = {
+        'monica@localhost' => {},
+        'brad@localhost'   => {},
+        'luke@localhost'   => {},
+        'ralph@localhost'  => {
+          'ensure' => 'absent',
+        },
+        'zack@localhost'   => {
+            'max_queries_per_hour' => 1200,
+        },
+    }
 
-  create_resources('mysql_user', $mysql_users, $mysql_defaults)
+    #create_resources('mysql_user', $mysql_users, $mysql_defaults)
+
+    $mysql_users.each | $name, $params | {
+        mysql_user { $name:
+          * => $params;
+        default:
+          * => $mysql_defaults;
+        }
+    }
 }
