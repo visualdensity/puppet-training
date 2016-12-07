ec7ba307c823fee633ff005eb7b7596851097129
wicked <wicked@puppetlabs.vm>
2016-12-07 04:38:21 +0000

    Additional notes

--- modules/kerberos/facts.d/wickedout.sh
+++ modules/kerberos/facts.d/wickedout.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# You execute this fact by calling the "key" part, which is `wicked_fact`
+#
+# So, once this is propagated, you would call it using this:
+#   facter -p wicked_fact
+#
+# Which would result in the output of:
+#  hahahahahhaha
+
 echo 'wicked_fact=hahahahahhaha'
